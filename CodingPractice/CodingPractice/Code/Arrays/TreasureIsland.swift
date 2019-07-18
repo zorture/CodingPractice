ba44cfc71bde48f9e54f9b49ce2b97d583f896fa
Kanwar Zorawar Singh Rana <>
2019-07-18 11:48:39 -0400

    Struct Hashable

--- CodingPractice/CodingPractice/Code/Arrays/TreasureIsland.swift
+++ CodingPractice/CodingPractice/Code/Arrays/TreasureIsland.swift
@@ -13,9 +13,13 @@ struct TreasureIsland {
     struct Loc: Hashable {
         let x: Int
         let y: Int
+        let index: Int
         func hash(into hasher: inout Hasher) {
-            hasher.combine(x.hashValue)
-            hasher.combine(y.hashValue)
+            hasher.combine(x)
+            hasher.combine(y)
+        }
+        static func == (lhs: Loc, rhs: Loc) -> Bool {
+            return lhs.x == rhs.x && lhs.y == rhs.y
         }
     }
     struct RouteQueue {
@@ -41,56 +45,52 @@ struct TreasureIsland {
     
     func shortestPath(forTreasure map: [[String]]) -> [Loc] {
         var shortestPathSet = Set<Loc>()
-        var shortestPathAr = [Loc]()
         var queue = RouteQueue()
-        queue.enqueue(loc: Loc(x: 0, y: 0))
+        var counter = 0
+        queue.enqueue(loc: Loc(x: 0, y: 0, index: counter))
         while (!queue.isEmpty()) {
             if let  loc = queue.peek(){
                 if loc.x < 3 && map[loc.y][loc.x+1] != "D" {
-                    let newLoc = Loc(x: loc.x+1, y: loc.y)
+                    let newLoc = Loc(x: loc.x+1, y: loc.y, index: counter)
                     if map[loc.y][loc.x+1] == "X" {
                         shortestPathSet.insert(loc)
-                        shortestPathAr.append(loc)
                         shortestPathSet.insert(newLoc)
-                        shortestPathAr.append(newLoc)
                         break
                     } else if !shortestPathSet.contains(newLoc){
                         queue.enqueue(loc: newLoc)
                     }
                 }
                 if loc.y < 3 && map[loc.y+1][loc.x] != "D" {
-                    let newLoc = Loc(x: loc.x, y: loc.y+1)
+                    let newLoc = Loc(x: loc.x, y: loc.y+1, index: counter)
                     if map[loc.y+1][loc.x] == "X" {
                         shortestPathSet.insert(loc)
-                        shortestPathAr.append(loc)
                         shortestPathSet.insert(newLoc)
-                        shortestPathAr.append(newLoc)
                         break
                     } else if !shortestPathSet.contains(newLoc){
                         queue.enqueue(loc: newLoc)
                     }
                 }
                 if loc.x > 0 && map[loc.y][loc.x-1] != "D" {
-                    let newLoc = Loc(x: loc.x-1, y: loc.y)
+                    let newLoc = Loc(x: loc.x-1, y: loc.y, index: counter)
                     if map[loc.y][loc.x-1] == "X" {
                         shortestPathSet.insert(loc)
-                        shortestPathAr.append(loc)
                         shortestPathSet.insert(newLoc)
-                        shortestPathAr.append(newLoc)
                         break
                     } else if !shortestPathSet.contains(newLoc){
                         queue.enqueue(loc: newLoc)
                     }
                 }
                 shortestPathSet.insert(loc)
-                shortestPathAr.append(loc)
                 queue.dequeue()
+                counter += 1
             }
         }
         
         var prev: Loc?
         var finalAr = [Loc]()
-        for loc in shortestPathAr.reversed(){
+        // sort on bases of index to backtrack from end to start
+        let shortestPathAr = shortestPathSet.sorted { $0.index > $1.index }
+        for loc in shortestPathAr{
             if prev != nil {
                 if (abs(prev!.x - loc.x) == 1 && abs(prev!.y - loc.y) == 0 ) ||
                     (abs(prev!.x - loc.x) == 0 && abs(prev!.y - loc.y) == 1) {
@@ -102,7 +102,6 @@ struct TreasureIsland {
                 prev = loc
             }
         }
-        
         return finalAr
     }
     
